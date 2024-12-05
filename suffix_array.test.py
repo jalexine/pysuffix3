c6167b7bbe71ab11a90e9a15be8aeba456374365
jalexine <127459834+jalexine@users.noreply.github.com>
2024-12-06 00:06:15 +0100

    updated to py3

--- suffix_array.test.py
+++ suffix_array.test.py
@@ -8,7 +8,7 @@ import tools_karkkainen_sanders as tks
 #s = open('Python.htm').read()
 s = 'ab'*10000
 #s = 'abab'
-s = unicode(s,'utf-8','replace')
+s = str(s,'utf-8','replace')
 n = len(s)
 ns, sa = tks.simple_kark_sort(s)
 lcp = tks.LCP(s,sa)
@@ -16,8 +16,8 @@ lcp = tks.LCP(s,sa)
 #print lcp
 #print sa
 
-for i in xrange(n-1) :
+for i in range(n-1) :
   if(s[sa[i]:] > s[sa[i+1]:]) :
-    print s[sa[i]:][:40]
-    print s[sa[i+1]:][:40]
-    print '='*50
+    print(s[sa[i]:][:40])
+    print(s[sa[i+1]:][:40])
+    print('='*50)
